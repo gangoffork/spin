48ee1f8d97827c1e90a52216a156b7e1af7fbaa5
FranciscoMarinho <francisco.marinho@gmail.com>
2016-06-08 17:26:50 -0300

    Another thinks!

--- js/appSpin.js
+++ js/appSpin.js
@@ -1,12 +1,62 @@
 'use strict';
-angular.module('appSpin', [])
-  .controller('appSpinController', ['$scope', '$http', function($scope, $http) {
-    $scope.appName = "Spin";
-    $scope.spinFooter = "Made by gangoffork.";
+angular.module('appSpin', ['ngRoute'])
+  .config(['$routeProvider', '$locationProvider',
+    function($routeProvider, $locationProvider) {
+      $routeProvider
+        .when('/home', {
+          templateUrl: 'pages/home.html',
+          controller: 'homeController',
+          controllerAs: 'home'
+        })
+        .when('/player', {
+          templateUrl: 'pages/player.html',
+          controller: 'playerController',
+          controllerAs: 'player'
+        })
+        .when('/login', {
+          templateUrl: 'pages/login.html',
+          controller: 'loginController',
+          controllerAs: 'login'
+        })
+        .otherwise({
+          redirectTo: '/home'
+        });
+      //$locationProvider.html5Mode(true);
+    }])
+  .controller('appSpinController', ['$scope', '$http', '$route', '$routeParams', '$location',
+    function($scope, $http, $route, $routeParams, $location) {
+      this.$route = $route;
+      this.$location = $location;
+      this.$routeParams = $routeParams;
 
+      $scope.appName = "Spin";
+      $scope.spinFooter = "Made by gangoffork.";
+      $scope.menu = [{name: "Player", uri: "#player"},{name: "Login", uri: "#login"}];
   }])
+  .controller('playerController', ['$scope','$routeParams', function($routeParams, $scope) {
+    // not yet implemented.
+    $scope.queue = [{name: "first", uri: "first.html"}, {name: "second", uri: "second.html"}];
+
+    console.log("player: " + $scope.queue);
+  }])
+  .controller('loginController', ['$routeParams', function($routeParams) {
+    // not yet implemented.
+  }])
+  .controller('homeController', ['$routeParams', function() {
+    // not yet implemented.
+  }])
+  .directive('spinHeader', function() {
+    return {
+      templateUrl: 'template/spin-header.html'
+    };
+  })
   .directive('spinFooter', function() {
     return {
       templateUrl: 'template/spin-footer.html'
+    };
+  })
+  .directive('spinMenu', function() {
+    return {
+      templateUrl: 'template/spin-menu.html'
     };
   });
